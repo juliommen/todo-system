755b4d043bf20e1fa657edb97b4032215d6ab257
Júlio Mendonça da Costa <juliommen@gmail.com>
2026-01-30 07:05:38 -0400

    change button hover when disabled

--- todo-web/src/shared/components/core/Button.tsx
+++ todo-web/src/shared/components/core/Button.tsx
@@ -10,14 +10,14 @@ export const Button = forwardRef<HTMLButtonElement, Props>(
     const base =
       "px-3 py-2 rounded-md font-medium focus:outline-none flex items-center gap-2 transition duration-200";
 
-    const disable = rest.disabled
-      ? "disabled:bg-green-700 disabled:cursor-not-allowed opacity-50"
+    const disabled = rest.disabled
+      ? "disabled:cursor-not-allowed opacity-50"
       : "";
 
     const cls =
       variant === "primary"
-        ? `${base} ${disable} bg-green-700 text-white hover:bg-green-800 `
-        : `${base} ${disable} bg-transparent border hover:border-gray-300 hover:text-gray-300`;
+        ? `${base} ${disabled} bg-green-700 text-white hover:bg-green-800 `
+        : `${base} ${disabled} bg-transparent border hover:border-gray-300 hover:text-gray-300`;
 
     return (
       <button ref={ref} className={`${cls}`} {...rest}>
